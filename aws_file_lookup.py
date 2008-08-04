4f76539d59d2de893d7e13b5b0d8bcb3200fb13a
Patrick Gallagher <rexaran@gmail.com>
2008-08-04 16:29:47 -0700

    added code to get rid of data processed entries

--- aws_file_lookup.py
+++ aws_file_lookup.py
@@ -7,11 +7,13 @@ ecs.setLicenseKey('1P5WQWG01FJRRNESE4G2')
 
 bookFile=open('/home/rexa/code/python/kate-look-up/kate-book-list-original.csv')
 bookStrings=bookFile.readlines()
-bookStrings=[entry for entry in bookStrings if entry!='\n']
-authorStrings=bookStrings[0::2]
-titleStrings=bookStrings[1::2]
-for title, author in zip(titleStrings,authorStrings):
-    print title,author
+#get rid of "Date Acquired:" entries
+bookStrings=[entry for entry in bookStrings if entry[0:4]!="Date"]
+authorStrings=bookStrings[0::3]
+titleStrings=bookStrings[1::3]
+for author, title in zip(authorStrings,titleStrings):
+    print title.lstrip('Title: '),author.lstrip('Author: ')
+    
     
 
 
