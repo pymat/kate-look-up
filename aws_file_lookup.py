179ee75e879d9fefa05aeb93b580e5170d6c7cf7
Patrick Gallagher <rexaran@gmail.com>
2008-08-04 17:32:20 -0700

    tried amazon search; poor results; trying google book search now

--- aws_file_lookup.py
+++ aws_file_lookup.py
@@ -7,25 +7,16 @@ ecs.setLicenseKey('1P5WQWG01FJRRNESE4G2')
 
 bookFile=open('/home/rexa/code/python/kate-look-up/kate-book-list-original.csv')
 bookStrings=bookFile.readlines()
+
 #get rid of "Date Acquired:" entries
 bookStrings=[entry for entry in bookStrings if entry[0:4]!="Date"]
+
 authorStrings=bookStrings[1::3]
 titleStrings=bookStrings[0::3]
+
 for author, title in zip(authorStrings,titleStrings):
-    print title.rstrip().strip('"').lstrip('Title: ')
-    print author.rstrip().strip('"').lstrip('Author: ')
-    
+    titleCln=title.rstrip().strip('"').lstrip('Title: ')
+    authorCln=author.rstrip().strip('"').lstrip('Author: ')
     
 
 
-#for fileDict in fileDictList:
-    #if fileDict['ISBNNum']:
-        #ISBNNum=fileDict['ISBNNum']
-        #ISBNNumsOnly=ISBNNum.replace('-','')
-        
-        #try:
-            #books = ecs.ItemLookup(ISBNNumsOnly,IdType='ISBN',SearchIndex='Books',ResponseGroup='ItemAttributes')
-        #except Exception, e:
-            #print e
-    
-        #topRes=books[0]
